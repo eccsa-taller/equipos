3808af6cfc19ddd87ffd4d8946ccefb376fcbce4
eccsa-taller <68203694+eccsa-taller@users.noreply.github.com>
2020-11-05 10:08:06 -0400

    Add files via upload

---
+++ blog/files/meta.js
@@ -0,0 +1,74 @@
+(function() {
+	var Realmac = Realmac || {};
+
+	Realmac.meta = {
+		
+		// Set the browser title
+		//
+		// @var String text
+		setTitle: function(text) {
+			return document.title = text;
+		},
+		
+		// Set the content attribute of a meta tag
+		//
+		// @var String name
+		// @var String content
+		setTagContent: function(tag, content){
+			// If the tag being set is title
+			// return the result of setTitle
+			if ( tag === 'title' )
+			{
+				return this.setTitle(content);
+			}
+			
+			// Otherwise try and find the meta tag
+			var tag = this.getTag(tag);
+			
+			// If we have a tag, set the content
+			if ( tag !== false )
+			{
+				return tag.setAttribute('content', content);
+			}
+			
+			return false;
+		},
+		
+		// Find a meta tag
+		//
+		// @var String name
+		getTag: function(name) {
+			var meta = document.querySelectorAll('meta');
+			
+			for ( var i=0; i<meta.length; i++ )
+			{
+				if (meta[i].name == name){
+					return meta[i];
+				}
+			}
+			
+			var tag = document.createElement('meta');
+			tag.name = name;
+			document.getElementsByTagName('head')[0].appendChild(tag);
+			
+			return tag;
+		}
+	};
+ 
+	// Object containing all website meta info
+	var websiteMeta = {"archive-november-2020.html":"Archives for November 2020","e3836e7b6c4a69d44a09f2e1b3213230-2.html":"La presente pagina WEB tiene por fin, el mantener información actualizada sobre los equipos de propiedad de la Empresa Constructora Cochabamba S.A. EC"};
+ 
+	// pageId must match the key in websiteMeta object
+	var url = window.location.pathname;
+	var pageId = url.substring(url.lastIndexOf('/')+1);
+	if (!pageId || pageId.length == 0){
+		pageId = 'index.html';
+	}
+	pageMeta = websiteMeta[pageId];
+ 
+	// If we have meta for this page
+	if (pageMeta){
+		Realmac.meta.setTagContent('description', pageMeta);
+	}
+ 
+ })();
